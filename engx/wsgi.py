13a868ee0e88adf69dd2b4854d94d95d5179bc26
alaa <eng.alaa.mohamed442@gmail.com>
2015-11-02 18:06:03 +0200

    fix wsgi file

--- engx/wsgi.py
+++ engx/wsgi.py
@@ -8,6 +8,19 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+project = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.insert(0, project)
+
+activate_this = os.path.join(project, '../engx_env/bin/activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
+
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "engx.settings"
+
+
 
 from django.core.wsgi import get_wsgi_application
 from mezzanine.utils.conf import real_project_name
